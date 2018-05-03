d048facae88aab59e2ed8cb4b3545dac6a9a609a
Reece Rodriguez <rtr266@nyu.edu>
2018-05-03 19:17:17 -0400

    add printBoard method

--- Battleship.java
+++ Battleship.java
@@ -6,6 +6,35 @@ import java.util.*;
 
 public class Battleship {
 
+    public static char[][] board=new char[10][10];
+            
+            
+    public static void printBoard(){
+    
+        System.out.print("  ");//beginning 2 spaces
+        for(int i = 0; i < board.length; ++i){
+            System.out.print(" " + (char)(i + 'A') +"  ");//print letters seperately.
+        }
+
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                board[i][j] = '-';  
+            }
+        }
+        for(int i = 0; i < board.length; i++){
+            System.out.println();
+            for(int j = 0; j < board.length; j++){
+                if(j == 0){
+                    System.out.print(i);
+                    System.out.print("| ");
+                }
+                System.out.print(board[i][j] + " | ");  
+            }
+        }
+        System.out.println("");
+    }   
+
+
     static int port = 5190;
     public static void main(String[] args) {
         ServerSocket server = null;
