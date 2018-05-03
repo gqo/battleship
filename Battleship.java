aea60575833eb936c7192636308fda5962a9c6ea
Graeme Ferguson <ggmferguson@gmail.com>
2018-05-03 18:26:39 -0400

    Add boilerplate chat server

---
+++ Battleship.java
@@ -0,0 +1,92 @@
+// Graeme Ferguson, Andrea Vasquez, Reece Rodriguez | Battleship | 05/03/18
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Battleship {
+
+    static int port = 5190;
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        int uid = 0; // User ID
+        try {
+            server = new ServerSocket(port);
+            // Handles only n-set connections, consider HashMap?
+            ArrayList<Connection> connections = new ArrayList<Connection>(20);
+            Connection conn = null;
+            while(true) {
+                System.out.println("Waiting for a connection on port: "+port);
+                Socket client = server.accept();
+                // Prints user, uid, and user's internet address to server
+                System.out.println("User "+(++uid)+" @ "
+                    +client.getInetAddress().toString()+" connected.");
+                conn = new Connection(client,uid,connections);
+                connections.add(conn);
+                conn.start();
+            }
+        } catch (IOException ex) {
+            System.out.println("Error occured in socket creation.");
+        }
+    }
+}
+
+// Class for connections made to the server
+class Connection extends Thread {
+    Socket client;
+    int uid;
+    ArrayList<Connection> connections = new ArrayList<Connection>(20);
+    PrintStream sout;
+    String u_name = "";
+
+    Connection(Socket new_client, int new_uid, ArrayList<Connection> new_conns) {
+        client = new_client;
+        uid = new_uid;
+        connections = new_conns;
+        try {
+            sout = new PrintStream(client.getOutputStream());
+        } catch (IOException ex) {
+            System.out.println("Error occured in PrintStream creation.");
+        }
+    }
+    
+    public void run() {
+        try {
+            Scanner sin = new Scanner(client.getInputStream());
+            String line = "";
+            String message = "";
+            boolean set_name = false;
+
+            while(!line.equals("/quit")) { // User can quit by sending "/quit" on the server
+                line = sin.nextLine();
+                if(!set_name) { 
+                    u_name = line;
+                    set_name = true;
+                    // Prints user's set username to server
+                    System.out.println("User "+uid+" set name to: "+line);
+                }
+                else {
+                    message =  u_name+": "+line;
+                    this.send(message); // Send message to user sending message
+                    for(int i = 0; i < connections.size(); i++) { // Sends message to all users
+                        if(i != uid-1) { connections.get(i).send(message); }
+                    }
+                    // Prints user sent messages to server
+                    System.out.println("User "+uid+" ("+u_name+")"+" said: "+line);
+                }
+            }
+            // Prints to server when user disconnects
+            System.out.println("User "+uid+" @ "+client.getInetAddress().toString()+" disconnected.");
+            sin.close();
+            client.close();
+        } catch (IOException ex) {
+            System.out.println("Error occured in scanner creation.");
+        }
+    }
+
+    // Allows for easy access to sout member variable in Connection
+    public void send(String message) { sout.println(message); }
+
+    // Returns string of Connection's internet address
+    public String getInetAddress() { return client.getInetAddress().toString(); }
+}
